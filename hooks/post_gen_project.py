c352ab0267dfdb88c39c1ba2365419b3e722f2c4
Neil Munro <neilmunro@gmail.com>
2018-10-22 21:35:25 +0100

    Added git pre-commit hook on project init

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,10 +1,19 @@
+from pathlib import Path
+from urllib import request
 import os
 import subprocess
-from urllib import request
 
+pre_commit = Path('.git', 'hooks', 'pre-commit')
 
 with open(os.devnull, 'w') as fp:
     subprocess.run(['git', 'init'], stdout=fp)
+
+with open(pre_commit, 'w') as f:
+    f.write('#!/usr/bin/env sh\n\n')
+    f.write('make lint && make test')
+
+with open(os.devnull, 'w') as fp:
+    subprocess.run(['chmod', '+x', str(pre_commit)], stdout=fp)
 
 request.urlretrieve(
     'https://www.gitignore.io/api/{{cookiecutter.project_gitignore_sets|replace(" ", ",")}}',
