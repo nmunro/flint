2f59ba143252baa68682f679321cea6b3891c404
Neil Munro <neilmunro@gmail.com>
2018-11-02 18:03:33 +0000

    Fixed documentation

--- {{cookiecutter.project_repo_name}}/docs/source/index.rst
+++ {{cookiecutter.project_repo_name}}/docs/source/index.rst
@@ -10,7 +10,7 @@ Welcome to {{cookiecutter.project_name}}'s documentation!
    :maxdepth: 2
    :caption: Contents:
 
-
+    {{cookiecutter.project_repo_name}}
 
 Indices and tables
 ==================
--- {{cookiecutter.project_repo_name}}/{{cookiecutter.project_repo_name}}/{{cookiecutter.project_repo_name}}.py
+++ {{cookiecutter.project_repo_name}}/{{cookiecutter.project_repo_name}}/{{cookiecutter.project_repo_name}}.py
@@ -1,5 +1,15 @@
-def main(*args, **kwargs):
-    print('Hello, world!')
+"""
+This is the meh project.
+"""
+
+from typing import Tuple, Dict
+
+def main(*args: Tuple, **kwargs: Dict):
+    """
+    :param Tuple args: Args to main
+    :param Dict kwargs: kwargs to main
+    """
+    print('Hello, Neil Munro!')
 
 
 if __name__ == '__main__':
