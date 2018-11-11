3586a09221716c88287873eccf8f7460b8ea8a64
Neil Munro <neilmunro@gmail.com>
2018-11-11 16:22:41 +0000

    Improved the Makefile and git pre-commit hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -9,8 +9,25 @@ with open(os.devnull, 'w') as fp:
     subprocess.run(['git', 'init'], stdout=fp)
 
 with open(pre_commit, 'w') as f:
-    f.write('#!/usr/bin/env sh\n\n')
-    f.write('make lint && make test')
+    f.write(
+        '#!/usr/bin/env sh\n\n'
+        '# Check requirements.txt exists\n'
+        'if [ ! -f requirements.txt ]; then\n'
+        '    echo "No requirements.txt file found, please write one!"\n'
+        '    exit 1\n'
+        'fi\n'
+        '\n'
+        '# Check requirements.txt is up to date\n'
+        'poetry show --no-dev | tr -s " " | sed \'s/ /==/\' | sed \'s/ .*//\' > .reqs.txt\n'
+        'if cmp -s requirements.txt .reqs.txt; then\n'
+        '    rm .reqs.txt\n'
+        '    make lint && make test\n'
+        'else\n'
+        '    echo "Outdated requirements.txt file found, please update it!"\n'
+        '    rm .reqs.txt\n'
+        '    exit 1\n'
+        'fi\n'
+    )
 
 with open(os.devnull, 'w') as fp:
     subprocess.run(['chmod', '+x', str(pre_commit)], stdout=fp)
